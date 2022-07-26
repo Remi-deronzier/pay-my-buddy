3d9148e305ea247f4474b3659fc212ad13e7b1c5
remax21 <deronzier.remi@gmail.com>
2022-07-26 19:55:47 +0200

    chore: add inital spring boot files
    Adds .gitignore, .mvn/, mvnw, mvnw.cmd, pom.xml and src/

---
+++ src/main/java/deronzier/remi/payMyBuddyV2/PayMyBuddyV2Application.java
@@ -0,0 +1,13 @@
+package deronzier.remi.payMyBuddyV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBuddyV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMyBuddyV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/deronzier/remi/payMyBuddyV2/PayMyBuddyV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package deronzier.remi.payMyBuddyV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBuddyV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
