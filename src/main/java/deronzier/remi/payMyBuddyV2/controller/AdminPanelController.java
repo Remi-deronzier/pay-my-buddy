cff643c1f48453a687ba78a331020fccee4ae813
remax21 <deronzier.remi@gmail.com>
2022-08-14 15:51:04 +0200

    style: rename 'panel admin' to 'admin panel' in all files concerned

--- src/main/java/deronzier/remi/payMyBuddyV2/controller/AdminPanelController.java
+++ src/main/java/deronzier/remi/payMyBuddyV2/controller/AdminPanelController.java
@@ -11,20 +11,20 @@ import deronzier.remi.payMyBuddyV2.service.impl.AccountServiceImpl;
 import deronzier.remi.payMyBuddyV2.service.impl.UserServiceImpl;
 
 @Controller
-@RequestMapping(value = "/panelAdmin")
-public class PanelAdminController {
+@RequestMapping(value = "/adminPanel")
+public class AdminPanelController {
 
 	@Autowired
 	private AccountServiceImpl accountService;
 
 	@GetMapping
-	public String getPanelAdmin(Model model) {
+	public String getAdminPanel(Model model) {
 		// Add user account to model
 		Account payMyBuddySuperUserAccount = accountService.findByUserId(UserServiceImpl.PAY_MY_BUDDY_SUPER_USER_ID)
 				.get();
 		model.addAttribute("account", payMyBuddySuperUserAccount);
 
-		return "panel-admin/view";
+		return "admin-panel/view";
 	}
 
 }
